7afaad54c00e3ba0ca1680f946de439f0ba1bb23
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-02-08 17:17:38 +0530

    Lecture-8

---
+++ Lecture-8/Bindings/script.js
@@ -0,0 +1,31 @@
+var hogwards = {
+    name:'Harry Potter',
+    getName : function(){
+        console.log(this.name) 
+    }
+}
+
+function fun(a,b){
+    console.log(this)
+    console.log(a)
+    console.log(b)
+}
+
+hogwards.getName()
+
+fun(10,20) 
+
+fun.call(hogwards,'a','b') 
+var x = fun.bind(hogwards)
+console.log('bind ka use krna')
+x('Hello',10)
+
+fun.apply(hogwards,['x','y'])
+
+
+function funGen(){
+    return 1;
+}
+
+let f = new funGen()
+
