0bc7cc8bee4e46ba8c03179d5361355613b2c73d
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-02-15 18:22:13 +0530

    Lecture-10

--- Lecture-10/fizzbuzz/script.js
+++ Lecture-10/fizzbuzz/script.js
@@ -5,14 +5,27 @@
 
 function printLoop(){
     let inp = document.getElementById('inp')
+    let list = document.getElementById('list')
+
     let N = parseInt(inp.value)
     console.log(typeof N)
-
+    let start = new Date().getTime()
     let str = ''
     for(let i=0;i<=N;i++){
-        str += i+' '
+        let x = ''
+        if(i%3==0) x+='fizz'
+        if(i%5==0) x+='buzz'
+        if(x=='') x = i
+        let li = document.createElement('li')
+        li.innerText = x
+        list.appendChild(li) 
+        // str += `<li>${x}</li>`
     }
-    console.log(str)
+    // list.innerHTML=str 
+    let end = new Date().getTime()
+    console.log(end-start)
+
+    // console.log(str)
 }
 
 // window.onload = function(){
